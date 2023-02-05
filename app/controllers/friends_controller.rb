b5d62efb18eba4a4b514e38b47001e224de0b684
oobersogoobers <alanireland.me@gmail.com>
2023-02-05 10:32:13 -0500

    Updating the Registration File to Prevent Error When Creating A New Account

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -24,7 +24,7 @@ class FriendsController < ApplicationController
 
   # POST /friends or /friends.json
   def create
-    #@friend = Friend.new(friend_params)
+    @friend = Friend.new(friend_params)
     @friend = current_user.friends.build(friend_params)
       
     respond_to do |format|
